488f07c2e5d8ee1116ee57b26c1de49aa7831425
Furkan Durceylan <furkandurceylan@gmail.com>
2026-08-13 14:51:32 +0300

    Add Dockerized frontend and update project configuration

--- LibApi/src/test/java/com/fdrcyln/LibApi/LibApiApplicationTests.java
+++ LibApi/src/test/java/com/fdrcyln/LibApi/LibApiApplicationTests.java
@@ -1,13 +1,13 @@
 package com.fdrcyln.LibApi;
 
+import com.fdrcyln.Starter.LibApiApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = LibApiApplication.class)
 class LibApiApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
